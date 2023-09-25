df2109ed7981ffb7f38942569a224f0c7afa5135
Dimitrije Radojkovic <dimitrijeradojkovic8@gmail.com>
2023-09-25 20:49:48 +0200

    FINAL

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import ToDo from "./ToDo";
 import "./style.css";
 
-export default function App(){
+const ToDoContext = createContext();
+
+let i = 0;
+
+function App(){
     const [text, setText] = useState("");
     const [toDo, setToDo] = useState([]);
 
+
     const onSubmitHandler = e => {
         e.preventDefault();
-        setToDo(value =>  [...value, <ToDo key={Math.floor(Math.random() * 256)} text={text} bg={`rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`} />]);
+        const newTask = {
+                id: i++,
+                key: Math.floor(Math.random() * 256),
+                text: text,
+                bg:`rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
+        }
+        setToDo(value =>  [...value, newTask]);
     }
 
     return (
         <main>
-            <div id="container">
-                {
-                    toDo.map(x => x)
-                }
-            </div>
+            <ToDoContext.Provider value={{toDo, setToDo}}>
+                <div id="container">
+                    {
+                        toDo.map(x => {
+                            
+                            return <ToDo
+                                key={x.key}
+                                id={x.id}
+                                text={x.text}
+                                bg={x.bg}
+                                className="container_todo"
+                            />
+                        })
+                    }
+                </div>
+            </ToDoContext.Provider>
             <form onSubmit={onSubmitHandler}>
                 <input type="text" onInput={e => {setText(e.target.value)}} />
                 <input type="submit" value="Napravi" />
             </form>
         </main>
     )
+}
+
+export {
+    App,
+    ToDoContext
 }
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,7 +1,40 @@
-const ToDo = ({text, bg}) => {
+import { useContext, useRef } from "react";
+import { ToDoContext } from "./App";
+
+const ToDo = ({id, text, bg}) => {
+    const {toDo, setToDo} = useContext(ToDoContext);
+
+    const deleteHandler = () => {
+        const updatedToDo = toDo.filter(x => x.id !== id);
+        setToDo(updatedToDo);
+        console.log(toDo);
+    }
+
+    const dragItem = useRef();
+    const dragOverItem = useRef();
+
+    const dragStart = (e, position) => {
+        e.dataTransfer.setData("text/plain", id.toString());
+    };
+
+    const dragEnter = (e, position) => {
+        e.preventDefault();
+    };
+
+    const drop = (e) => {
+        e.preventDefault();
+        const dragItemId = e.dataTransfer.getData("text/plain");
+        const dragItemIndex = parseInt(dragItemId, 10);
+        const updatedToDo = [...toDo];
+        const [draggedItem] = updatedToDo.splice(dragItemIndex, 1);
+        updatedToDo.splice(id, 0, draggedItem);
+        setToDo(updatedToDo);
+    };
+
     return (
-        <div className="container_todo" style={{backgroundColor: bg}}>
+        <div onDragStart={(e) => dragStart(e, id)} onDragEnter={(e) => dragEnter(e, id)} onDragEnd={drop}  style={{backgroundColor: bg}} id={id} draggable>
             <p>{text}</p>
+            <button onClick={deleteHandler}>Obrisi</button>
         </div>
     )
 }
